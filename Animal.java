4f00bb19e4c3dedd82cff7bbed515930035d99aa
sithumsamaraweera <259830838+sithumsamaraweera@users.noreply.github.com>
2026-08-17 16:42:25 +0530

    Add Class and Objects lesson

---
+++ Animal.java
@@ -0,0 +1,7 @@
+public class Animal //Creating Class named Animal
+{
+    public static void main(String[] args){
+        Animal cat = new Animal();   // Creates a new Animal object and assigns its reference to 'cat
+
+    }
+}
